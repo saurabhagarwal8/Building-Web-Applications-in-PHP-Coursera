006de47d175ce568e21280ce5dd5989febfff043
saurabhagarwal8 <saurabh28a@gmail.com>
2020-04-10 22:59:04 +0530

    Week - 2,3,5,6 assignments

---
+++ Week 5/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Saurabh Agarwal PHP</title>
+</head>
+<body>
+    <h1>Saurabh Agarwal PHP</h1>
+    <pre>ASCII ART:
+    ************
+    ************
+    **
+    **
+    ************
+    ************
+              **
+              **
+    ************
+    ************
+    </pre>
+    <p>
+        The SHA256 hash of "Saurabh Agarwal" is <?php print hash('sha256', 'Saurabh Agarwal'); ?>
+    </p>
+    <?php ?>
+</body>
+</html>
